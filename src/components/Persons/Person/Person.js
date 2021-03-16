b159fc16b5b70275be92d2d97c1d87b22b601a22
FedorowPavel <pasha_fedorow@mail.ru>
2021-03-16 11:40:31 +0200

    project structure

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import classes from './Person.css';
-// import styled from 'styled-components';
 
 
 const person = (props) => {
 
-     const rnd = Math.random();
-    
-        if (rnd > 0.7) {
-            throw new Error ('Something went wrong')
-        }
-    
     return (
-       
-    
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErorrBoundary/ErrorBoundary'
+import Person from '../components/Persons/Person/Person';
   
 
 class App extends Component {
@@ -55,11 +54,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}><Person
+            return <Person
+                    key={person.id}
                     click={() => this.deletePersonHandler(index)}
                     name={person.name}
                     age={person.age}
-                    changed={(event) => this.nameChangedHandler(event, person.id)} /></ErrorBoundary>
+                    changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       )
